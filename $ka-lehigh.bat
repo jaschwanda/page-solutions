e8ec77e358638ae75cf5446c1b02127b273794fb
Jim Schwanda <iJAS@usi2solve.com>
2019-06-22 23:14:09 -0400

    Delete $ka-lehigh.bat

--- $ka-lehigh.bat
+++
@@ -1,2 +0,0 @@
-copy *.php "D:\Users\JAS\My Web Sites\Clients\www.ka-lehigh.org\public_html\secret\temple\wp-content\plugins\usi-page-solutions"
-copy usi-library\*.php "D:\Users\JAS\My Web Sites\Clients\www.ka-lehigh.org\public_html\secret\temple\wp-content\plugins\usi-page-solutions\usi-library"
