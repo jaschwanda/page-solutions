c0727a6c6e5170ada315851a3b55805255afe76c
jaschwanda <iJAS@usi2solve.com>
2020-01-16 05:56:49 -0500

    Added license and copyright notice.

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,25 @@
 <?php // ------------------------------------------------------------------------------------------------------------------------ //
 
+/*
+Page-Solutions is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ 
+Page-Solutions is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License along with Page-Solutions. If not, see 
+https://github.com/jaschwanda/Page-solutions/blob/master/LICENSE.md
+
+Copyright (c) 2020 by Jim Schwanda.
+*/
+
 require_once(plugin_dir_path(__DIR__) . 'usi-wordpress-solutions/usi-wordpress-solutions-uninstall.php');
 
 require_once('usi-page-solutions.php');
 
 final class USI_Page_Solutions_Uninstall {
 
-   const VERSION = '1.4.0 (2019-11-27)';
+   const VERSION = '1.5.0 (2020-01-12)';
 
    private function __construct() {
    } // __construct();
