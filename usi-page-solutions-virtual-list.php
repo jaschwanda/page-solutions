09281d5a3b43ef43c02fb73e5a545792bf95de82
jaschwanda <iJAS@usi2solve.com>
2020-01-17 05:35:32 -0500

    Added license and copyright notice.

--- usi-page-solutions-virtual-list.php
+++ usi-page-solutions-virtual-list.php
@@ -1,5 +1,18 @@
 <?php // ------------------------------------------------------------------------------------------------------------------------ //
 
+/*
+Page-Solutions is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ 
+Page-Solutions is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License along with Page-Solutions. If not, see 
+https://github.com/jaschwanda/Page-solutions/blob/master/LICENSE.md
+
+Copyright (c) 2020 by Jim Schwanda.
+*/
+
 defined('ABSPATH') or die('Accesss not allowed.');
 
 if (!class_exists('WP_List_Table')) {
@@ -8,7 +21,7 @@ if (!class_exists('WP_List_Table')) {
 
 class USI_Page_Solutions_Virtual_List extends WP_List_Table {
 
-   const VERSION = '1.4.0 (2019-11-27)';
+   const VERSION = '1.5.0 (2020-01-12)';
 
    private $delete_virtual = false;
    private $edit_virtual = false;
