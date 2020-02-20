c98152af3445ccb0b0f8a2a24eefe3084794f760
jaschwanda <iJAS@usi2solve.com>
2020-02-20 00:42:07 -0500

    Updated capability and updates handling, set all versions numbers to same version.

--- uninstall.php
+++ uninstall.php
@@ -19,7 +19,7 @@ require_once('usi-page-solutions.php');
 
 final class USI_Page_Solutions_Uninstall {
 
-   const VERSION = '1.5.2 (2020-02-06)';
+   const VERSION = '1.5.3 (2020-02-20)';
 
    private function __construct() {
    } // __construct();
--- usi-page-dbs-mysqli.php
+++ usi-page-dbs-mysqli.php
@@ -18,7 +18,7 @@ Extends the standard mysqli database access functions by providing the following
 
 class USI_Page_Dbs extends mysqli {
 
-   const VERSION = '1.5.2 (2020-02-06) [USI_Dbs:1.1.3 (2018-01-18)]';
+   const VERSION = '1.5.3 (2020-02-20) [USI_Dbs:1.1.3 (2018-01-18)]';
 
    const PREPARE_ERROR_1 = '; check the manual that corresponds to your MySQL server version for the right syntax to use';
 
